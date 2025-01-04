c1609c88c89249075e2fd61c8359dcbda61018ce
atarsander <szymczyk.alek1@gmail.com>
2025-01-04 17:38:22 +0100

    Created lower bound restriction checking for population generation.

--- models/individual.py
+++ models/individual.py
@@ -2,6 +2,7 @@ from deap import creator, base, tools
 from santas_workshop_tour.data_grabber import DataGrabber
 from santas_workshop_tour.config import FAMILY_DATA
 import random
+import heapq
 
 
 def make_preferences_dict(preferences):
@@ -11,6 +12,33 @@ def randomize_preferences_order(preferences):
     preferences_dict = make_preferences_dict(preferences)
     shuffled_items = random.sample(list(preferences_dict.items()), len(preferences_dict))
     return dict(shuffled_items)
+
+def verify_lower_bound(family_choices, visits_day, visitors_by_days):
+    while True:
+        all_verified = True
+        for day, visitors in visitors_by_days.items():
+            switch = False
+            if visitors < 125:
+                all_verified = False
+                ten_largest = heapq.nlargest(10, visitors_by_days.items(), key=lambda item: item[1])
+                ten_largest_days = [i[0] for i in ten_largest]
+                for family_id, preference in enumerate(visits_day):
+                    if preference in ten_largest_days:
+                        if day in family_choices.get_family_choices(family_id):
+                            visitors_by_days[preference] -= family_choices.get_family_size(family_id)
+                            visitors_by_days[day] += family_choices.get_family_size(family_id)
+                            visits_day[family_id] = day
+                            switch = True
+                if not switch:
+                    day_to_swap = random.choice(ten_largest_days)
+                    visitors_by_days[day_to_swap] -= family_choices.get_family_size(family_id)
+                    visitors_by_days[day] += family_choices.get_family_size(family_id)
+                    visits_day[family_id] = day
+        if all_verified:
+            return visits_day, visitors_by_days
+
+def check_visiting_restriction(family_size, preference, visitors_by_days):
+    return visitors_by_days[preference] + family_size < 301
 
 def init_individual(family_choices):
     visits_day = [-1 for _ in range(5000)]
@@ -35,11 +63,8 @@ def init_individual(family_choices):
             visits_day[family_id] = least_occupied_day
             visitors_by_days[least_occupied_day] += family_size
 
+    visits_day, visitors_by_days = verify_lower_bound(family_choices, visits_day, visitors_by_days)
     return visits_day, visitors_by_days
-
-def check_visiting_restriction(family_size, preference, visitors_by_days):
-    return visitors_by_days[preference] + family_size < 301
-
 
 def create_population(family_choices, N):
     creator.create("FitnessMax", base.Fitness, weights=(1.0,)) #temporary for testing
@@ -48,4 +73,5 @@ def create_population(family_choices, N):
     toolbox.register("individual", tools.initIterate, creator.Individual, lambda: init_individual(family_choices))
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     return toolbox.population(n=N)
+
 
