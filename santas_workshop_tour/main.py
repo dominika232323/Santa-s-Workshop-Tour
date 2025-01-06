80b02a56d6fb7ea9aaf0890f626de85566e0076e
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-06 18:46:51 +0100

    Initiate main.py script that calls evolutionary algorithm

---
+++ santas_workshop_tour/main.py
@@ -0,0 +1,10 @@
+from santas_workshop_tour.config import FAMILY_DATA
+from santas_workshop_tour.data_grabber import DataGrabber
+from santas_workshop_tour.evolutionary_algorithm import EvolutionaryAlgorithm
+
+
+if __name__ == '__main__':
+    grabber = DataGrabber(FAMILY_DATA)
+    algorithm = EvolutionaryAlgorithm(grabber, 0.7, 0.05, 0.02, 40, 30)
+    result = algorithm(2, 50)
+    print(result)
