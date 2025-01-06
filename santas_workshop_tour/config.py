8a2b0520ca1cef1217653c30ddc49bfc5a7f0f3a
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-06 19:44:53 +0100

    Add RESULTS_DIR

--- santas_workshop_tour/config.py
+++ santas_workshop_tour/config.py
@@ -6,6 +6,9 @@ PROJ_ROOT = Path(__file__).resolve().parents[1]
 
 DATA_DIR = PROJ_ROOT / "data"
 FAMILY_DATA = DATA_DIR / "family_data.csv"
+
+RESULTS_DIR = PROJ_ROOT / "results"
+
 Individual = Tuple[List[int], Dict[int, int]]
 
 
