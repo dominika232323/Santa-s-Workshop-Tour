61dae85a5431bdab163f0988f8a839cfb3de845e
atarsander <szymczyk.alek1@gmail.com>
2025-01-06 01:58:33 +0100

    Changed calculations to vectorized variant making it faster.

--- santas_workshop_tour/cost_function.py
+++ santas_workshop_tour/cost_function.py
@@ -1,35 +1,32 @@
-import deap
 from santas_workshop_tour.data_grabber import DataGrabber
-from santas_workshop_tour.individual import Individual
+from santas_workshop_tour.config import Individual
+import numpy as np
 
 
-def cost_function(self, family_choices: DataGrabber, individual: Individual) -> float:
-    restriction_penalty = self.calculate_restriction_penalty(individual)
-    choice_penalty = self.calculate_choice_penalty(family_choices, individual)
-    accounting_penalty = self.calculate_accounting_penalty(individual)
-    print(
-        "restriction_penalty:",
-        restriction_penalty,
-        "choice_penalty:",
-        choice_penalty,
-        "accounting_penalty",
-        accounting_penalty,
-    )
+def cost_function(individual: Individual, family_choices: DataGrabber) -> float:
+    restriction_penalty = calculate_restriction_penalty(individual)
+    choice_penalty = calculate_choice_penalty(individual, family_choices)
+    accounting_penalty = calculate_accounting_penalty(individual)
+    # print(
+    #     "restriction_penalty:",
+    #     restriction_penalty,
+    #     "choice_penalty:",
+    #     choice_penalty,
+    #     "accounting_penalty",
+    #     accounting_penalty,
+    # )
     return (restriction_penalty + choice_penalty + accounting_penalty,)
 
 
 def calculate_restriction_penalty(individual: Individual) -> int:
     restriction_penalty = 0
-    for chosen_day in individual[0]:
-        visitors_by_days = individual[1]
-        if not (125 <= visitors_by_days[chosen_day] < 301):
-            restriction_penalty += 100000
+    visitors_counts = np.array(list(individual[1].values()))
+    restriction_penalty = np.sum((visitors_counts < 125) | (visitors_counts > 300)) * 100000
 
     return restriction_penalty
 
 
-def calculate_choice_penalty(family_choices: DataGrabber, individual: Individual) -> int:
-
+def calculate_choice_penalty(individual: Individual, family_choices: DataGrabber) -> int:
     choice_penalty = 0
     choices_penalty_scheme = {
         0: (0, 0),
@@ -45,16 +42,15 @@ def calculate_choice_penalty(family_choices: DataGrabber, individual: Individual
         10: (500, 434),
     }
     for family_id, chosen_day in enumerate(individual[0]):
-        chosen = False
-        for choice, day in enumerate(family_choices.get_family_choices(family_id)):
-            if day == chosen_day:
-                chosen = True
-                choice_penalty += (
+        choices = np.array(family_choices.get_family_choices(family_id))
+        match = np.where(choices == chosen_day)[0]
+        if match.size > 0:
+            choice = match[0]
+            choice_penalty += (
                     choices_penalty_scheme[choice][0]
                     + family_choices.get_family_size(family_id) * choices_penalty_scheme[choice][1]
                 )
-
-        if not chosen:
+        else:
             choice_penalty += (
                 choices_penalty_scheme[10][0]
                 + family_choices.get_family_size(family_id) * choices_penalty_scheme[10][1]
@@ -64,17 +60,11 @@ def calculate_choice_penalty(family_choices: DataGrabber, individual: Individual
 
 
 def calculate_accounting_penalty(individual: Individual) -> float:
-    accounting_penalty = 0
-    visitors_by_days = individual[1]
-    prev_visitors = visitors_by_days[100]
-    print(visitors_by_days)
+    visitors_by_days = np.array(list(individual[1].values()))
+    prev_visitors = np.roll(visitors_by_days, shift=1)
+    prev_visitors[0] = visitors_by_days[-1]
 
-    for visitors in reversed(visitors_by_days.values()):
-        print(abs(visitors - prev_visitors))
-        accounting_penalty += max(
-            (visitors - 125.0) / 400.0 * visitors ** (0.5 + abs(visitors - prev_visitors) / 50.0),
-            0,
-        )
-        prev_visitors = visitors
-
-    return max(0, accounting_penalty)
+    diff = np.abs(visitors_by_days-prev_visitors)
+    penalty = np.sum((visitors_by_days - 125.0)/400 * visitors_by_days**(0.5 + diff/50.0))
+    accounting_penalty = np.sum(penalty)
+    return max(accounting_penalty, 0)
