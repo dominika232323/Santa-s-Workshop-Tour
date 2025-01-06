a2ab2a934b0be2227d7b7941f9a78620935ef941
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-06 23:00:55 +0100

    Add saving results to files

--- santas_workshop_tour/main.py
+++ santas_workshop_tour/main.py
@@ -1,11 +1,20 @@
-from santas_workshop_tour.config import FAMILY_DATA
+from datetime import datetime
+
+from loguru import logger
+
+from santas_workshop_tour.config import FAMILY_DATA, RESULTS_DIR
 from santas_workshop_tour.data_grabber import DataGrabber
 from santas_workshop_tour.evolutionary_algorithm import EvolutionaryAlgorithm
-
+from santas_workshop_tour.results_handler import save_results
 
 if __name__ == '__main__':
+    current_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    logger.info(f"Current timestamp: {current_timestamp}")
+
     grabber = DataGrabber(FAMILY_DATA)
     algorithm = EvolutionaryAlgorithm(grabber, 0.7, 0.05, 0.02, 40, 30)
-    result, time = algorithm(2, 50)
-    print(result)
-    print(time)
+    best_individual, time = algorithm(2, 50)
+
+    result_path = RESULTS_DIR / current_timestamp
+    logger.info(f"Saving results to {result_path}")
+    save_results(best_individual, time, result_path)
--- santas_workshop_tour/results_handler.py
+++ santas_workshop_tour/results_handler.py
@@ -1,3 +1,6 @@
+import csv
+import json
+
 from loguru import logger
 from pathlib import Path
 
@@ -5,13 +8,40 @@ from santas_workshop_tour.config import Individual
 
 
 def save_results(best_individual: Individual, time: float, path_to_save: Path) -> None:
-    write_time_to_text_file(time, path_to_save)
+    save_list_to_csv(best_individual[0], path_to_save / "assigned_days.csv", ["family_id", "assigned_day"])
+    save_dict_to_json(best_individual[1], path_to_save / "people_per_day.json")
+    write_value_to_text_file(best_individual.fitness.values[0], path_to_save / "individual_value.txt")
+    write_value_to_text_file(time, path_to_save / "time.txt")
 
 
-def write_time_to_text_file(time: float, path_to_save: Path) -> None:
-    path_to_save.mkdir(parents=True, exist_ok=True)
+def write_value_to_text_file(value: float, path_to_txt: Path) -> None:
+    path_to_txt.parent.mkdir(parents=True, exist_ok=True)
 
-    file_path = path_to_save / f"time.txt"
+    logger.info(f"Writing value ({value:.4f}) to text file at {path_to_txt}")
+    path_to_txt.write_text(str(value))
 
-    logger.info(f"Writing time ({time:.4f} secs) to text file at {file_path}")
-    file_path.write_text(str(time))
+
+def save_list_to_csv(data: list[int], path_to_csv: Path, headers: list[str]) -> None:
+    if len(headers) != 2:
+        raise ValueError(f"Expected 2 headers, got {len(headers)}")
+
+    path_to_csv.parent.mkdir(parents=True, exist_ok=True)
+
+    logger.info(f"Writing list to csv at {path_to_csv}")
+
+    with path_to_csv.open(mode="w", newline="") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(headers)
+
+        for family_id, assigned_day in enumerate(data):
+            writer.writerow([family_id, assigned_day])
+
+
+def save_dict_to_json(data: dict, path_to_json: Path) -> None:
+    path_to_json.parent.mkdir(parents=True, exist_ok=True)
+
+    logger.info(f"Writing dict to json at {path_to_json}")
+
+    with open(path_to_json, mode="w") as file:
+        json.dump(data, file, indent=4)
