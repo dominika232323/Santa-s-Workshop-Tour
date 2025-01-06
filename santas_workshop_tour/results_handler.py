97c389d106a75e34048fdf2aabd3b408b187ae59
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-06 19:57:44 +0100

    Add write_time_to_text_file function

--- santas_workshop_tour/results_handler.py
+++ santas_workshop_tour/results_handler.py
@@ -1,13 +1,17 @@
+from loguru import logger
 from pathlib import Path
 
 from santas_workshop_tour.config import Individual
 
 
-def save_results(best_individual: Individual, time: float, path_to_save: Path, timestamp: str) -> None:
-    write_float_to_text_file(time, path_to_save, timestamp)
+def save_results(best_individual: Individual, time: float, path_to_save: Path) -> None:
+    write_time_to_text_file(time, path_to_save)
 
 
-def write_float_to_text_file(value: float, path_to_save: Path, timestamp: str) -> None:
+def write_time_to_text_file(time: float, path_to_save: Path) -> None:
     path_to_save.mkdir(parents=True, exist_ok=True)
-    file_path = path_to_save / f"{timestamp}.txt"
-    file_path.write_text(str(value))
+
+    file_path = path_to_save / f"time.txt"
+
+    logger.info(f"Writing time ({time:.4f} secs) to text file at {file_path}")
+    file_path.write_text(str(time))
