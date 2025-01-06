110e1200277224ea1b8c79728fb7df848eddfefb
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-06 23:48:08 +0100

    Move writing to files functions to file_io.py

--- santas_workshop_tour/results_handler.py
+++ santas_workshop_tour/results_handler.py
@@ -1,11 +1,7 @@
-import csv
-import json
-from math import isclose
-
-from loguru import logger
 from pathlib import Path
 
 from santas_workshop_tour.config import Individual
+from santas_workshop_tour.files_io import write_value_to_text_file, save_list_to_csv, save_dict_to_json
 
 
 def save_results(best_individual: Individual, time: float, path_to_save: Path) -> None:
@@ -13,39 +9,6 @@ def save_results(best_individual: Individual, time: float, path_to_save: Path) -
     save_dict_to_json(best_individual[1], path_to_save / "people_per_day.json")
     write_value_to_text_file(best_individual.fitness.values[0], path_to_save / "fitness_function_value.txt")
     write_value_to_text_file(time, path_to_save / "time.txt")
-
-
-def write_value_to_text_file(value: float, path_to_txt: Path) -> None:
-    path_to_txt.parent.mkdir(parents=True, exist_ok=True)
-
-    logger.info(f"Writing value ({value:.4f}) to text file at {path_to_txt}")
-    path_to_txt.write_text(str(value))
-
-
-def save_list_to_csv(data: list[int], path_to_csv: Path, headers: list[str]) -> None:
-    if len(headers) != 2:
-        raise ValueError(f"Expected 2 headers, got {len(headers)}")
-
-    path_to_csv.parent.mkdir(parents=True, exist_ok=True)
-
-    logger.info(f"Writing list to csv at {path_to_csv}")
-
-    with path_to_csv.open(mode="w", newline="") as file:
-        writer = csv.writer(file)
-
-        writer.writerow(headers)
-
-        for family_id, assigned_day in enumerate(data):
-            writer.writerow([family_id, assigned_day])
-
-
-def save_dict_to_json(data: dict, path_to_json: Path) -> None:
-    path_to_json.parent.mkdir(parents=True, exist_ok=True)
-
-    logger.info(f"Writing dict to json at {path_to_json}")
-
-    with open(path_to_json, mode="w") as file:
-        json.dump(data, file, indent=4)
 
 
 def is_result_valid(path_to_result: Path) -> bool:
