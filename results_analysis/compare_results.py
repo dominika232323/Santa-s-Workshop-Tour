2877aa14aaf971f095578ee22ccae58db6927a4a
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-12 19:03:35 +0100

    Make results as optional argument

--- results_analysis/compare_results.py
+++ results_analysis/compare_results.py
@@ -12,32 +12,35 @@ app = typer.Typer()
 
 
 @app.command()
-def main(results: list[Path]):
+def main(results: list[Path] = typer.Argument(None, help="List of result directories")):
     current_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     logger.info(f"Current timestamp: {current_timestamp}")
 
-    scores = {}
-    best_result_path = None
-    best_result_score = None
+    if not results:
+        print("No results found")
+    else:
+        scores = {}
+        best_result_path = None
+        best_result_score = None
 
-    for result_path in results:
-        people_per_day_dict = read_json(result_path / "people_per_day.json")
+        for result_path in results:
+            people_per_day_dict = read_json(result_path / "people_per_day.json")
 
-        if is_result_valid(people_per_day_dict):
-            fitness_function_value = read_value_from_txt_file(result_path / "fitness_function_value.txt")
-            time = read_value_from_txt_file(result_path / "time.txt")
+            if is_result_valid(people_per_day_dict):
+                fitness_function_value = read_value_from_txt_file(result_path / "fitness_function_value.txt")
+                time = read_value_from_txt_file(result_path / "time.txt")
 
-            result_score = get_result_score(fitness_function_value, time)
-            scores[str(result_path)] = result_score
+                result_score = get_result_score(fitness_function_value, time)
+                scores[str(result_path)] = result_score
 
-            if best_result_path is None or result_score > best_result_score:
-                best_result_path = result_path
-                best_result_score = result_score
+                if best_result_path is None or result_score > best_result_score:
+                    best_result_path = result_path
+                    best_result_score = result_score
 
-    save_dict_to_json(scores, RESULTS_COMPARISONS / current_timestamp / "scores.json")
+        save_dict_to_json(scores, RESULTS_COMPARISONS / current_timestamp / "scores.json")
 
-    best_result = {str(best_result_path): best_result_score}
-    save_dict_to_json(best_result, RESULTS_COMPARISONS / current_timestamp / "best_result.json")
+        best_result = {str(best_result_path): best_result_score}
+        save_dict_to_json(best_result, RESULTS_COMPARISONS / current_timestamp / "best_result.json")
 
 
 if __name__ == "__main__":
