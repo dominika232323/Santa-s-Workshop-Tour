aeaf8fa27f78abfd6c55c6c502e6254b74e252d6
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-29 23:50:20 +0100

    New results

--- results/generate_latex.py
+++ results/generate_latex.py
@@ -149,5 +149,5 @@ def print_section_comparison(timestamp, timestamps):
 
 if __name__ == "__main__":
     timestamps = print_sections_test()
-    print_section_comparison("20250129_231225", timestamps)
+    print_section_comparison("20250129_234258", timestamps)
 
